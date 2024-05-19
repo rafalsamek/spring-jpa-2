ba3a811fee2b3d36f704403fce3687170dae36da
Mateusz Wręczycki <setsudan.hana@gmail.com>
2024-05-19 09:37:05 +0200

    Todo crud controller

---
+++ src/main/java/pl/edu/wszib/springjpa/controller/TodoController.java
@@ -0,0 +1,44 @@
+package pl.edu.wszib.springjpa.controller;
+
+import org.springframework.web.bind.annotation.*;
+import pl.edu.wszib.springjpa.model.Todo;
+import pl.edu.wszib.springjpa.service.TodoService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    private final TodoService service;
+
+    public TodoController(TodoService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Todo> list() {
+        return service.list();
+    }
+
+    @GetMapping("/{id}")
+    public Todo get(@PathVariable Integer id) {
+        return service.get(id);
+    }
+
+    @PostMapping
+    public Todo create(@RequestBody Todo todo) {
+        return service.create(todo);
+    }
+
+    @PutMapping("/{id}")
+    public Todo update(@PathVariable Integer id, @RequestBody Todo todo) {
+        todo.setId(id);
+        return service.update(todo);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        service.delete(id);
+    }
+}
